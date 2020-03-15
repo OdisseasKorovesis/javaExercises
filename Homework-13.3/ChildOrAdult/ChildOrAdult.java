547fef91082e69b2447050e4374ce602044f5e79
OdisseasKorovesis <62053602+OdisseasKorovesis@users.noreply.github.com>
2020-03-15 11:06:09 +0200

    Add files via upload

---
+++ Homework-13.3/ChildOrAdult/ChildOrAdult.java
@@ -0,0 +1,16 @@
+
+package childoradult;
+
+
+public class ChildOrAdult {
+
+    
+    public static void main(String[] args) {
+        int age = 25;
+        
+        String message = (age<18)?"You are a child!":"You are an adult";
+        System.out.println(message);
+                
+    }
+    
+}
