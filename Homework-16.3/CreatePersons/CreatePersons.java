92c1613936791865298f99c57e598699c4f7027a
OdisseasKorovesis <62053602+OdisseasKorovesis@users.noreply.github.com>
2020-03-17 23:28:14 +0200

    Add files via upload

--- Homework-16.3/CreatePersons/CreatePersons.java
+++ Homework-16.3/CreatePersons/CreatePersons.java
@@ -13,7 +13,7 @@ public class CreatePersons {
         Scanner sc = new Scanner(System.in);
         String filename = "C:\\Users\\Odisseas KD\\Desktop\\Coding\\Bootcamp\\TerminalExercises\\familyFile.txt";
         File file = new File(filename);
-        String initialMessage = "NAME \t AGE \t HEIGHT(m) \t WEIGHT(kg)";
+        String initialMessage = String.format("%-15s%-5s%-8s%-8s", "NAME", "AGE", "HEIGHT", "WEIGHT");
         FileWriter fr = new FileWriter(file, true);
         PrintWriter pr = new PrintWriter(fr);
         pr.print(initialMessage);
@@ -45,44 +45,39 @@ public class CreatePersons {
         String name = sc.next();
         
         int age;
-        System.out.println("What is his/her age?");
         do{
-            
+            System.out.println("What is his/her age? (0-100)");
             while(!sc.hasNextInt()) {
             System.out.println("Please enter a number.");
-            sc.next();
+            sc.next(); 
         }
-            System.out.println("Please enter a valid age.");
             age = sc.nextInt();
         }while(age<0 || age >100);   
         
 
-        double height;
-        System.out.println("What is his/her height?");
+        double height;        
         do{
-            
+            System.out.println("What is his/her height? (0-2.40)");
             while(!sc.hasNextDouble()) {
             System.out.println("Please enter a number.");
             sc.next();
         }
-            System.out.println("Please enter a valid height.");
             height = sc.nextDouble();
-        }while(height<0 || age >2.40);
+        }while(height<0 || height>2.40); 
         
-        double weight;
-        System.out.println("What is his/her weight?");
+        double weight;        
         do{
-            
+            System.out.println("What is his/her weight? (0-200.0)");
             while(!sc.hasNextDouble()) {
             System.out.println("Please enter a number.");
             sc.next();
         }
-            System.out.println("Please enter a valid weight.");
             weight = sc.nextDouble();
-        }while(weight<0 || age >200);
+        }while(weight<0 || weight>200);  
         
-        
-        pr.append("\n" + name + " \t" + age + " \t" + height + " \t" + weight);
+        pr.append("\n");
+        String toPrint = String.format("%-15s%-5d%-8.2f%-8.2f", name, age, height, weight);
+        pr.append(toPrint);
         fr.close();
         pr.close();
         System.out.println("Person succesfully created.");
